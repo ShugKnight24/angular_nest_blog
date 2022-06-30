81656e5dd78915ab905a63336692cf98d6d06a4b
Shugmi Shumunov <SShumunov@gmail.com>
2022-06-30 19:59:03 -0400

    feat: Add authentication with JWT and password hashing

--- api/src/user/models/user.interface.ts
+++ api/src/user/models/user.interface.ts
@@ -1,5 +1,15 @@
+export interface LoginError {
+  error: string;
+}
+
+export interface LoginSuccess {
+  access_token: string;
+}
+
 export interface User {
   id?: number;
   name?: string;
   username?: string;
+  email?: string;
+  password?: string;
 }
